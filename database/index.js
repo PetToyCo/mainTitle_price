c10b24b87fdafb5d5bbca0494e78248a8a059b28
Elizabeth Hayt <enhayt@gmail.com>
2020-06-22 12:57:40 -0700

    Create mongoose db connection and Price schema

---
+++ database/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mainTitle_price');
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('error connecting to mainTitle_price db');
+});
+
+db.once('open', () => {
+  console.log('connected to mainTitle_price db');
+});
+
+const priceSchema = mongoose.Schema({
+  price: {
+    type: Number,
+    required: true
+  },
+  currency: String
+});
+
+const Price = mongoose.model('Price', priceSchema);
+
+module.exports.Price = Price;
