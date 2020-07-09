0c24df97ea30f4731b2e6800b46179c8cc87bb5f
Elizabeth Hayt <enhayt@gmail.com>
2020-07-09 10:43:35 -0700

    Add axios request for db data from this service

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 
 class MainTitle extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      brand: '',
+      title: '',
+      rating: '',
+      reviews: '',
+      currency: '',
+      price: ''
     }
+  }
+
+  //hardcoded to item 100 for development
+  componentDidMount() {
+    var item = 100;
+
+    //axios request for currency, price
+    axios.get(`itemPrice/${item}`)
+    .then(data => {
+      console.log('success getting data in componentDidMount: ', data);
+      this.setState({
+        currency: data.data.currency,
+        price: data.data.price
+      });
+    })
+    .catch(err => {
+      console.log('error getting descObj in componentDidMount: ', err);
+    });
+
+    //axios request for brand, title
+    axios.get(`http://127.0.0.1/descriptionObject/${item}`)
+    .then(data => {
+      console.log('success getting data in componentDidMount: ', data);
+      this.setState({
+        currency: data.data.currency,
+        price: data.data.price
+      });
+    })
+    .catch(err => {
+      console.log('error getting descObj in componentDidMount: ', err);
+    });
+
+    //axios request for stars and # of reviews
   }
 
 
