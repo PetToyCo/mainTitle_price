17c5a5f30b9d597e9af0f810dfc6f5521aa2a7ae
Elizabeth Hayt <enhayt@gmail.com>
2020-07-09 12:43:38 -0700

    Complete basic layout of service

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -21,51 +21,85 @@ class MainTitle extends React.Component {
 
     //axios request for currency, price
     axios.get(`http://127.0.0.1:3005/itemPrice/${item}`)
-    .then(data => {
-      console.log('success getting itemPrice');
-      this.setState({
-        currency: data.data.currency,
-        price: data.data.price
+      .then(data => {
+        console.log('success getting itemPrice');
+        this.setState({
+          currency: data.data.currency,
+          price: data.data.price
+        });
+      })
+      .catch(err => {
+        console.log('error getting itemPrice in componentDidMount: ', err);
       });
-    })
-    .catch(err => {
-      console.log('error getting itemPrice in componentDidMount: ', err);
-    });
 
     //axios request for brand, title
     axios.get(`http://127.0.0.1:3002/descriptionObject/${item}`)
-    .then(data => {
-      console.log('success getting descriptionObject');
-      this.setState({
-        brand: data.data.description.primaryBrand,
-        title: data.data.description.title
+      .then(data => {
+        console.log('success getting descriptionObject');
+        this.setState({
+          brand: data.data.description.primaryBrand,
+          title: data.data.description.title
+        });
+      })
+      .catch(err => {
+        console.log('error getting descObj in componentDidMount: ', err);
       });
-    })
-    .catch(err => {
-      console.log('error getting descObj in componentDidMount: ', err);
-    });
 
     //axios request for stars and # of reviews
     axios.get(`http://127.0.0.1:3001/averageReviews/${item}`)
-    .then(data => {
-      console.log('success getting averageReviews: ', data);
-      this.setState({
-        rating: data.data.reviewAverage,
-        reviews: data.data.numberOfReviews
+      .then(data => {
+        console.log('success getting averageReviews: ', data);
+        this.setState({
+          rating: data.data.reviewAverage,
+          reviews: data.data.numberOfReviews
+        });
+      })
+      .catch(err => {
+        console.log('error getting averageReviews in componentDidMount: ', err);
       });
-    })
-    .catch(err => {
-      console.log('error getting averageReviews in componentDidMount: ', err);
-    });
   }
 
 
 
   render() {
     return (
-     <div id='mainTitle'>
-       <h1>Title</h1>
-     </div> 
+      <div id='mainTitle'>
+        <div id='info'>
+          {/* note to add L and W */}
+          <h1>{`${this.state.title}`}</h1>
+          <div id='by'>By: <span>{this.state.brand}</span></div>
+          <div id='wrapper'>
+            <div id='stars'>
+              <span>Star</span>
+              <span>Star</span>
+              <span>Star</span>
+              <span>Star</span>
+              <span>Star</span>
+              <span>{`(${this.state.reviews})`}</span>
+            </div>
+            <div id='price'>
+              <span>{this.state.currency}</span>
+              <span>{this.state.price}</span>
+            </div>
+          </div>
+          <div id='bottomSection'>
+            <div id='promo'>
+              <span>Price Match Guarentee Image </span>
+              <span>Details</span>
+            </div>
+            <div id='qty'>
+              <form>
+                <div id='form'>
+                  <label>Quantity</label>
+                  <input type='button' value='-' />
+                  <input type='text' value='1' />
+                  <input type='button' value='+' />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
