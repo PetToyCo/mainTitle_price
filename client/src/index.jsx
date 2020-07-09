945f075a8833297ea52dca07d13d2b9460201725
Elizabeth Hayt <enhayt@gmail.com>
2020-07-09 12:25:13 -0700

    Completed axios calls for all needed data for service

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -20,33 +20,43 @@ class MainTitle extends React.Component {
     var item = 100;
 
     //axios request for currency, price
-    axios.get(`itemPrice/${item}`)
+    axios.get(`http://127.0.0.1:3005/itemPrice/${item}`)
     .then(data => {
-      console.log('success getting data in componentDidMount: ', data);
+      console.log('success getting itemPrice');
       this.setState({
         currency: data.data.currency,
         price: data.data.price
       });
     })
     .catch(err => {
-      console.log('error getting descObj in componentDidMount: ', err);
+      console.log('error getting itemPrice in componentDidMount: ', err);
     });
 
     //axios request for brand, title
     axios.get(`http://127.0.0.1:3002/descriptionObject/${item}`)
     .then(data => {
-      console.log('success getting data in componentDidMount: ', data);
+      console.log('success getting descriptionObject');
       this.setState({
         brand: data.data.description.primaryBrand,
         title: data.data.description.title
       });
-      console.log(this.state);
     })
     .catch(err => {
       console.log('error getting descObj in componentDidMount: ', err);
     });
 
     //axios request for stars and # of reviews
+    axios.get(`http://127.0.0.1:3001/averageReviews/${item}`)
+    .then(data => {
+      console.log('success getting averageReviews: ', data);
+      this.setState({
+        rating: data.data.reviewAverage,
+        reviews: data.data.numberOfReviews
+      });
+    })
+    .catch(err => {
+      console.log('error getting averageReviews in componentDidMount: ', err);
+    });
   }
 
 
