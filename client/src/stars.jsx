470e7bf2053de1875ef4e7f0fc742d459a86afa1
Elizabeth Hayt <enhayt@gmail.com>
2020-07-16 12:59:06 -0700

    Update tests and add tests for Stars component

--- client/src/stars.jsx
+++ client/src/stars.jsx
@@ -4,24 +4,21 @@ import { blackStars, halfStars, whiteStars, ratingNum } from './style.js';
 var Stars = (props) => (
   <div id='starsSection'>
     <span id='stars'>
-    {props.black > 0 && <span style={blackStars}></span>}
-    {props.black > 1 && <span style={blackStars}></span>}
-    {props.black > 2 && <span style={blackStars}></span>}
-    {props.black > 3 && <span style={blackStars}></span>}
-    {props.black > 4 && <span style={blackStars}></span>}
-    {props.half > 0 && <span style={halfStars}></span>}
-    {props.white > 0 && <span style={whiteStars}></span>}
-    {props.white > 1 && <span style={whiteStars}></span>}
-    {props.white > 2 && <span style={whiteStars}></span>}
-    {props.white > 3 && <span style={whiteStars}></span>}
-    {props.white > 4 && <span style={whiteStars}></span>}
+    {props.black > 0 && <span id='b1' style={blackStars}></span>}
+    {props.black > 1 && <span id='b2' style={blackStars}></span>}
+    {props.black > 2 && <span id='b3' style={blackStars}></span>}
+    {props.black > 3 && <span id='b4' style={blackStars}></span>}
+    {props.black > 4 && <span id='b5' style={blackStars}></span>}
+    {props.half > 0 && <span id='h1' style={halfStars}></span>}
+    {props.white > 0 && <span id='w1' style={whiteStars}></span>}
+    {props.white > 1 && <span id='w2' style={whiteStars}></span>}
+    {props.white > 2 && <span id='w3' style={whiteStars}></span>}
+    {props.white > 3 && <span id='w4' style={whiteStars}></span>}
+    {props.white > 4 && <span id='w5' style={whiteStars}></span>}
     </span>
     <span id='numOfReviews' style={ratingNum}>{`(${props.reviews})`}</span>
   </div>
 )
-
-
-
 
 
 export default Stars;
